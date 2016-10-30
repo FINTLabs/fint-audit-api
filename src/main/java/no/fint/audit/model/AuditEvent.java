0bd74a5d4ec3824ee263a1fc7b6e5310fb45740c
Frode Sjovatsen <frode@sjovatsen.net>
2016-10-30 20:25:06 +0100

    made model serializable

--- src/main/java/no/fint/audit/model/AuditEvent.java
+++ src/main/java/no/fint/audit/model/AuditEvent.java
@@ -2,8 +2,10 @@ package no.fint.audit.model;
 
 import lombok.Data;
 
+import java.io.Serializable;
+
 @Data
-public class AuditEvent {
+public class AuditEvent implements Serializable {
     private String corrId;
     private String source;
     // TODO: 29.10.2016 Need to change the datatype from Object to FINT event object when it is finished 
